d29adf79bb205599f3aaacfe7634c42e4c8d7fdb
Krzysztof Łapan <lapankrz@interia.pl>
2022-04-08 20:44:12 +0200

    Added filling missing longitude, latitude and postal codes based on address.

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import geocoder
+import requests
 
 class Preprocessing(object):
     """
@@ -10,7 +11,7 @@ class Preprocessing(object):
         self.df = self.read_data(path)
 
     def read_data(self, path):
-        return pd.read_csv(path, on_bad_lines = "skip")
+        return pd.read_csv(path, error_bad_lines=False)
 
     def trim_redundant_data(self):
         columns_to_drop = [
@@ -20,9 +21,20 @@ class Preprocessing(object):
             'Borough',
             'New Georeferenced Column'
         ]
-        return self.df.drop(columns = columns_to_drop)
+        return self.df.drop(columns=columns_to_drop)
 
+    def fill_missing_geodata(self):
+        with requests.Session() as session:
+            for index, row in self.df.iterrows():
+                if pd.isnull(row['POSTCODE']) and not pd.isnull(row['STADDR']):
+                    g = geocoder.osm(row['STADDR'] + ', New York, NY', session=session)
+                    if (g is not None):
+                        row['POSTCODE'] = g.postal
+                        row['Latitude'] = g.lat
+                        row['Longitude'] = g.lng
+                        print(index)
 
     def run(self):
         self.df = self.trim_redundant_data()
+        self.fill_missing_geodata()
         return self.df
