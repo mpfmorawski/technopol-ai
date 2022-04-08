0fcde8c091efcf72028607242e00e4850e2f3a10
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-04-08 20:59:41 +0200

    add_columns_with_distances_to_main_places implementation

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -20,9 +20,41 @@ class Preprocessing(object):
             'Borough',
             'New Georeferenced Column'
         ]
-        return self.df.drop(columns = columns_to_drop)
+        self.df = self.df.drop(columns = columns_to_drop)
 
+    def add_lat_to_central_park_column(self):
+        central_park_lat = 40.785091
+        self.df['lat_to_central_park'] = self.df['Latitude'] - central_park_lat
+
+    def add_long_to_central_park_column(self):
+        central_park_long = -73.968285
+        self.df['long_to_central_park'] = self.df['Longitude'] - central_park_long
+
+    def add_lat_to_times_square_column(self):
+        times_square_lat = 40.758896
+        self.df['lat_to_times_square'] = self.df['Latitude'] - times_square_lat
+
+    def add_long_to_times_square_column(self):
+        times_square_long = -73.985130
+        self.df['long_to_times_square'] = self.df['Longitude'] - times_square_long
+
+    def add_lat_to_financial_district_column(self):
+        financial_district_lat = 40.7075
+        self.df['lat_to_financial_district'] = self.df['Latitude'] - financial_district_lat
+
+    def add_long_to_financial_district_column(self):
+        financial_district_long = -74.009167
+        self.df['long_to_financial_district'] = self.df['Longitude'] - financial_district_long
+
+    def add_columns_with_distances_to_main_places(self):
+        self.add_lat_to_central_park_column()
+        self.add_long_to_central_park_column()
+        self.add_lat_to_times_square_column()
+        self.add_long_to_times_square_column()
+        self.add_lat_to_financial_district_column()
+        self.add_long_to_financial_district_column()
 
     def run(self):
-        self.df = self.trim_redundant_data()
+        self.trim_redundant_data()
+        self.add_columns_with_distances_to_main_places()
         return self.df
