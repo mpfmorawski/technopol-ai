dc23f6babf52730f48b6caf5534d3ec08f6a20d7
adamnapieralski <adam.napieralski@gmail.com>
2022-04-08 21:01:47 +0200

    add replace_lengths_with_areas() & fill_missing_column_values_with_mean_for_boro()

--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 class Preprocessing(object):
@@ -25,4 +26,35 @@ class Preprocessing(object):
 
     def run(self):
         self.df = self.trim_redundant_data()
+        self.replace_lengths_with_areas()
         return self.df
+
+    def get_mean_column_value_for_boro(self, data, column_name: str, boro: int, include_zeros: bool = False):
+        column_vals = data[data['BORO'] == boro][column_name]
+        if include_zeros:
+            return np.mean(column_vals)
+        else:
+            return np.mean(column_vals[column_vals != 0])
+
+    def fill_missing_column_values_with_mean_for_boro(self, data, column_name: str):
+        mean_area_per_boro = {}
+        for boro in data['BORO'].unique():
+            mean_area_per_boro[boro] = self.get_mean_column_value_for_boro(data, column_name, boro)
+
+        for i, row in data.iterrows():
+            data.at[i, column_name] = mean_area_per_boro[row['BORO']]
+
+    def replace_lengths_with_areas(self):
+        lot_f = 'LTFRONT'
+        lot_d = 'LTDEPTH'
+        bld_f = 'BLDFRONT'
+        bld_d = 'BLDDEPTH'
+        self.df['LTAREA'] = self.df[lot_f] * self.df[lot_d]
+        self.df['BLDAREA'] = self.df[bld_f] * self.df[bld_d]
+        self.df['LTAREA'] = np.where(self.df['LTAREA'] == 0, self.df['BLDAREA'], self.df['LTAREA'])
+        self.df['BLDAREA'] = np.where(self.df['BLDAREA'] == 0, self.df['LTAREA'], self.df['BLDAREA'])
+
+        self.df.drop(columns=[lot_f, lot_d, bld_f, bld_d])
+
+        self.fill_missing_column_values_with_mean_for_boro(self.df, 'LTAREA')
+        self.fill_missing_column_values_with_mean_for_boro(self.df, 'BLDAREA')
